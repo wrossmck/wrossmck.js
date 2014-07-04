ddc086d3ebe7df9831da1520a5e65aa8810ab784
Ross McKinley <wrossmck@gmail.com>
2014-07-04 15:02:20 +0100

    truncates long tweets
    and tidies up output

--- index.js
+++ index.js
@@ -11,22 +11,25 @@ var twit = new twitter(auth);
 var s = fs.createReadStream(__dirname + '/text.txt');
 m.seed(s, function () {
 	var stdin = process.openStdin();
+	util.print('[Hello] Enter some text to generate a resonse');
 	util.print('> ');
 	
 	// a new markov response (and tweet) will be made after each line on the stdin. 
 	stdin.on('data', function (line) {
 		var res = m.respond(line.toString()).join(' ');
 		res = res.replace(/[\u0250-\ue007]/g, '');//removes some strange characters
-		console.log(res);
+		console.log("[Response ]"+res);
+		if(res.length() > 140){
+			res = res.substring(0,140);// truncates to 140 characters
+			console.log("[Truncated] "+res);
+		}
 		// send req to twitter
 		if(res.trim()!==""){
 			twit.verifyCredentials(function (err, data) {
 				if(err){console.log(err);}
-				// console.log(data);
 			}).updateStatus(res,
 				function (err, data) {
 				if(err){console.log(err);}
-					// console.log(data);
 				}
 			);
 		}
